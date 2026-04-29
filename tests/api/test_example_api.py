efcc737eb3a4e04aa5817f354bf2661a18453f4c
Natallia Krotsik <n.krotsik@andersenlab.com>
2026-04-29 09:36:02 +0000

    Update test_example_api.py

--- tests/api/test_example_api.py
+++ tests/api/test_example_api.py
@@ -1,3 +1,4 @@
+# tests/api/test_example_api.py
 import allure
 import pytest
 from framework.api.loans_api import LoansAPI
@@ -9,7 +10,7 @@ from framework.api.loans_api import LoansAPI
 class TestBanksProductsAndServices:
 
     @allure.severity(allure.severity_level.NORMAL)
-    def test_check_the_loans_list(self, get_tokens):
-        loans_api = LoansAPI(get_tokens.get("id_token"))
+    def test_check_the_loans_list(self, auth_tokens):
+        loans_api = LoansAPI(auth_tokens.get("id_token"))
         loans = loans_api.get_all_loans_of_user()
         assert loans.status_code == 200
